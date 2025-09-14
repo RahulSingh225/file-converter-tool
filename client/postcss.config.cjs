9ec256a210cd8d7e534c126f594240a78e871199
Rahul <rahulsingh793@gmail.com>
2025-09-15 01:31:04 +0530

    reset

--- client/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// PostCSS configuration using the @tailwindcss/postcss plugin and autoprefixer
-// Ensures the newer Tailwind PostCSS plugin is used when installed.
-module.exports = {
-  plugins: [
-    // Use the package installed: @tailwindcss/postcss
-    require('@tailwindcss/postcss')(),
-    require('autoprefixer')(),
-  ],
-};
